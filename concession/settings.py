37a4ebe4c93131e33ec5dbe449eeb777348623a0
sooraj <tpsooraj7@gmail.com>
2025-01-21 12:52:26 +0530

    csrf error

--- concession/settings.py
+++ concession/settings.py
@@ -133,6 +133,11 @@ USE_I18N = True
 
 USE_TZ = True
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://concession.onrender.com',
+    'http://localhost:8000',
+    'http://127.0.0.1:8000',
+]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
